a1dea8a95b0da0cdabea2a55706b9d76bc1aacd3
JUN HYEOK KIM <kjh45388117@gmail.com>
2022-10-20 17:48:28 +0900

    recat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/*islint-disable */
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  /**대충 서버에서 가져왔다고 치기 */
+  let logo = "나만의 작은 블로그✨";
+  let [title, chtitle] = useState([
+    "남자 코트 추천",
+    "가을 코트 추천",
+    "농구 코트 추천",
+  ]);
+  let [like, pluslike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <h4>{logo}</h4>
+      </div>
+      <div className="list">
+        <button>가나다순 정렬</button>
+        <h4>
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              pluslike(like + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {like}
+          <button
+            onClick={() => {
+              let titlecopy = [...title];
+              titlecopy[0] = "여자 코트 추천";
+              chtitle(titlecopy);
+            }}
+          >
+            제목 수정
+          </button>
+        </h4>
+        <p>10월 15일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>10월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>10월 17일 발행</p>
+      </div>
     </div>
   );
 }
